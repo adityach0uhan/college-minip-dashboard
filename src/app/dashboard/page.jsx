81cffb09503097b702fca6f138f499d2097a255f
adityach0uhan <m4with6x@gmail.com>
2024-06-17 18:00:51 +0530

    Added Demo charts

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,9 +1,38 @@
+"use client"
 import React from 'react'
+import { BarChart } from '@mui/x-charts/BarChart';
+import { PieChart } from "@mui/x-charts/PieChart";
+const page = () => {const data = [
+  { id: 0, value: 10, label: "series A" },
+  { id: 1, value: 15, label: "series B" },
+  { id: 2, value: 20, label: "series C" },
+];
 
-const page = () => {
   return (
-    <div className='w-full h-full bg-yellow-300'>Dashboard</div>
-  )
+    <div className="w-full h-full ">
+      <BarChart
+        xAxis={[{ scaleType: "band", data: ["Grocery", "Dairy", "Drinks"] }]}
+        series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
+        width={500}
+        height={300}
+      />
+      <PieChart
+        series={[
+          {
+            data,
+            highlightScope: { faded: "global", highlighted: "item" },
+            faded: { innerRadius: 30, additionalRadius: -30, color: "gray" },
+          },
+        ]}
+        height={200}
+      />
+    </div>
+  );
 }
 
 export default page
+
+
+
+
+
--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import Sidebar from "../components/global/Sidebar";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="max-w-screen max-h-screen overflow-hidden bg-blue-100">
+      <body className="max-w-screen max-h-screen overflow-hidden bg-zinc-100">
         <div className="md:hidden w-screen h-screen px-6 flex justify-center items-center text-lg">Use Laptop/Destop or Enable Desktop Mode in your browser </div>
         <div className=" hidden md:flex w-full h-full ">
           <Sidebar />
