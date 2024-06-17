427ca9e9b22de269f3fc2aede97fdd511e8a9bc6
adityach0uhan <m4with6x@gmail.com>
2024-06-17 16:18:46 +0530

    Added Mobile Device Warning

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="max-w-screen max-h-screen overflow-hidden bg-blue-100">
-        
+        <div className="md:hidden w-screen h-screen px-6 flex justify-center items-center text-lg">Use Laptop/Destop or Enable Desktop Mode in your browser </div>
         <div className=" hidden md:flex w-full h-full ">
           <Sidebar />
           <div className="p-2 w-full h-full ">{children}</div>
