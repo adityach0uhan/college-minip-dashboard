d38dfc43084216d6af9ac23a129d13534b40c7fe
adityach0uhan <m4with6x@gmail.com>
2024-06-17 16:05:04 +0530

    Addeed Sidebar

---
+++ src/components/global/Sidebar.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Home, CalendarClock, CalendarDays, ShoppingCart, Info, LogOut } from "lucide-react";
+import Link from "next/link";
+
+const Sidebar = () => {
+  return (
+    <div className="w-48 h-screen bg-white text-black p-2 ">
+      <div className="text-3xl">Sales Prediction</div>
+      <ul className="w-full h-3/5   mt-10 flex items-start  gap-5 justify-center flex-col">
+        <li className="w-full h-1/5 flex justify-start items-center pl-2 rounded-lg hover:bg-blue-300 p-2 ">
+          <Link
+            className="flex w-full h-full items-center gap-5"
+            href="/dashboard"
+          >
+            <Home />
+            Dashboard
+          </Link>
+        </li>
+        <li className="w-full h-1/5 flex justify-start items-center pl-2 rounded-lg hover:bg-blue-300 p-2 ">
+          <Link className="flex w-full h-full items-center gap-5" href="">
+            <CalendarDays />
+            Daily
+          </Link>
+        </li>
+        <li className="w-full h-1/5 flex justify-start items-center  pl-2 rounded-lg hover:bg-blue-300 p-2 ">
+          <Link className="flex w-full h-full items-center gap-5" href="">
+            <CalendarClock />
+            Monthly
+          </Link>
+        </li>
+        <li className="w-full h-1/5 flex justify-start items-center  pl-2 rounded-lg hover:bg-blue-300 p-2 ">
+          <Link className="flex w-full h-full items-center gap-5" href="">
+            <ShoppingCart />
+            Inventory
+          </Link>
+        </li>
+        <li className="w-full h-1/5 flex justify-start items-center pl-2 rounded-lg hover:bg-blue-300 p-2 ">
+          <Link className="flex w-full h-full items-center gap-5" href="">
+            <Info />
+            Tips
+          </Link>
+        </li>
+        <li className="w-full h-1/5 flex justify-start items-center pl-2 rounded-lg hover:bg-blue-300 p-2 ">
+          <Link className="flex w-full h-full items-center gap-5" href="">
+            <LogOut />
+            Logout
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
