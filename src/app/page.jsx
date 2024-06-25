840e2286bee3a8e6dc4bd1d3e54e0a2b63ae8586
adityach0uhan <m4with6x@gmail.com>
2024-06-25 21:39:34 +0530

    Some Changes in Main Page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,70 +1,77 @@
-// "use client";
-// import * as React from "react";
-// import { BarChart } from "@mui/x-charts/BarChart";
-// import { useState, useEffect } from "react";
-// import sample from "@/data/sample";
-// import InputLabel from "@mui/material/InputLabel";
-// import MenuItem from "@mui/material/MenuItem";
-// import FormControl from "@mui/material/FormControl";
-// import Select, { SelectChangeEvent } from "@mui/material/Select";
-// const valueFormatter = (value) => value.toFixed(1); // Format the value to two decimal places
-
-// export default function Home() {
-//    const [products, setProducts] = useState('');
-
-//   const handleChange = (event) => {
-//     setProducts(event.target.value );
-//   };
-//   const series = [
-//     { dataKey: "yhat_lower", label: "Lowest Probability", valueFormatter },
-//     { dataKey: "yhat", label: "Prediction", valueFormatter },
-//     { dataKey: "yhat_upper", label: "Highest Probability", valueFormatter },
-//   ];
-
-//   const chartDataset = sample.map((data) => ({
-//     ...data,
-//     ds: data.ds, 
-//   }));
-
-//   return (
-//     <>
-//       <div className="w-full h-12 ">
-//         <div className="w-28">
-          
-//         <FormControl fullWidth>
-//           <InputLabel id="demo-simple-select-label">Products</InputLabel>
-//           <Select
-//             labelId="demo-simple-select-label"
-//             id="demo-simple-select"
-//             value={products}
-//             label="Products"
-//             onChange={handleChange}
-//             >
-//             <MenuItem value={10}>Grocery</MenuItem>
-//             <MenuItem value={20}>Dairy</MenuItem>
-//             <MenuItem value={30}>Thirty</MenuItem>
-//           </Select>
-//         </FormControl>
-//             </div>
-//       </div>
-
-//       <BarChart
-//         dataset={chartDataset}
-//         xAxis={[{ scaleType: "band", dataKey: "ds" }]}
-//         series={series}
-//         yAxis={[{ label: "Value" }]}
-//         width={1200}
-//         height={400}
-//       />
-//     </>
-//   );
-// }
-import React from 'react'
-
+import React from 'react';
+import Link from 'next/link';
+import { Button } from '@mui/material';
+import { SendIcon } from 'lucide-react';
 const page = () => {
-  return (
-    <div>Abe Monthly Check Kar</div>
-  )
-}
+    return (
+        <>
+            <div className=' w-full h-96 flex gap-4 justify-center flex-wrap '>
+                <div className='w-80 h-1/2 mt-10 bg-yellow-50'>
+                    <div class='cursor-pointer transition-all duration-500 hover:translate-y-2 w-full h-full bg-neutral-50 rounded-lg shadow-xl flex flex-row items-center justify-evenly gap-8 px-4'>
+                        <img height={70} width={70} src='/weeks.svg' alt='' />
+                        <div>
+                            <div class='font-bold mb-3 text-purple-300'>
+                                Daily
+                            </div>
+                            <div class='line-clamp-3 '>
+                                <div>View Daily Product Data</div>
+                                <br />
+                                <Button variant='contained'>
+                                    <Link
+                                        className='w-full items-center justify-around h-full flex gap-2'
+                                        href='/Monthly'>
+                                        Daily Data <SendIcon />
+                                    </Link>
+                                </Button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='w-80 h-1/2 mt-10 bg-yellow-50'>
+                    <div class='cursor-pointer transition-all duration-500 hover:translate-y-2 w-full h-full bg-neutral-50 rounded-lg shadow-xl flex flex-row items-center justify-evenly gap-8 px-4'>
+                        <img height={70} width={70} src='/weeks.svg' alt='' />
+                        <div>
+                            <div class='font-bold mb-3 text-purple-300'>
+                                Weekly
+                            </div>
+                            <div class='line-clamp-3 '>
+                                <div>View Weekly Product Data</div>
+                                <br />
+                                <Button variant='contained'>
+                                    <Link
+                                        className='w-full items-center justify-around h-full flex gap-2'
+                                        href='/Monthly'>
+                                        Weekly Data <SendIcon />
+                                    </Link>
+                                </Button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='w-80 h-1/2 mt-10 bg-yellow-50'>
+                    <div class='cursor-pointer transition-all duration-500 hover:translate-y-2 w-full h-full bg-neutral-50 rounded-lg shadow-xl flex flex-row items-center justify-evenly gap-8 px-4'>
+                        <img height={70} width={70} src='/weeks.svg' alt='' />
+                        <div>
+                            <div class='font-bold mb-3 text-purple-300'>
+                                Weekly
+                            </div>
+                            <div class='line-clamp-3 '>
+                                <div>View Monthly Product </div>
+                                <br />
+                                <Button variant='contained'>
+                                    <Link
+                                        className='w-full items-center justify-around h-full flex gap-2'
+                                        href='/Monthly'>
+                                        Monthly Data <SendIcon />
+                                    </Link>
+                                </Button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
 
-export default page
+export default page;
