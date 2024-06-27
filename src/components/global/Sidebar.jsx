24710f6913ae96799d766093a2a5b6427eb1a34f
adityach0uhan <m4with6x@gmail.com>
2024-06-27 13:31:13 +0530

    Improved Sidebar Code

--- src/components/global/Sidebar.jsx
+++ src/components/global/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
     Home,
     CalendarClock,
@@ -6,56 +6,55 @@ import {
     CalendarMinus2,
     LogOut
 } from 'lucide-react';
-import Link from "next/link";
+import Link from 'next/link';
 
 const Sidebar = () => {
-  return (
-      <div className='w-48 h-full sticky  bg-white text-black p-2 '>
-          <div className='text-3xl'>Sales Prediction</div>
-          <ul className='w-full h-96    mt-10 flex items-start  gap-5 justify-center flex-col'>
-              <li className='w-full h-10 flex justify-start items-center pl-2 rounded-lg hover:bg-blue-300 p-2 '>
-                  <Link
-                      className='flex w-full h-full items-center gap-5'
-                      href='/'>
-                      <Home />
-                      Home
-                  </Link>
-              </li>
-              <li className='w-full h-10 flex justify-start items-center pl-2 rounded-lg hover:bg-blue-300 p-2 '>
-                  <Link
-                      className='flex w-full h-full items-center gap-5'
-                      href='/Daily'>
-                      <CalendarDays />
-                      Daily
-                  </Link>
-              </li>
-              <li className='w-full h-10 flex justify-start items-center  pl-2 rounded-lg hover:bg-blue-300 p-2 '>
-                  <Link
-                      className='flex w-full h-full items-center gap-5'
-                      href='/Monthly'>
-                      <CalendarClock />
-                      Monthly
-                  </Link>
-              </li>
-              <li className='w-full h-10 flex justify-start items-center pl-2 rounded-lg hover:bg-blue-300 p-2 '>
-                  <Link
-                      className='flex w-full h-full items-center gap-5'
-                      href='/Weekly'>
-                      <CalendarMinus2 />
-                      Weekly
-                  </Link>
-              </li>{' '}
-              <li className='w-full h-10 flex justify-start items-center pl-2 rounded-lg hover:bg-blue-300 p-2 '>
-                  <Link
-                      className='flex w-full h-full items-center gap-5'
-                      href=''>
-                      <LogOut />
-                      Logout
-                  </Link>
-              </li>
-          </ul>
-      </div>
-  );
+    const data = [
+        {
+            route: '/',
+            icon: <Home />,
+            title: 'Home'
+        },
+        {
+            route: '/Daily',
+            icon: <CalendarDays />,
+            title: 'Daily'
+        },
+        {
+            route: '/Weekly',
+            icon: <CalendarMinus2 />,
+            title: 'Weekly'
+        },
+        {
+            route: '/Monthly',
+            icon: <CalendarClock />,
+            title: 'Monthly'
+        },
+        {
+            route: '/logout',
+            icon: <LogOut />,
+            title: 'Logout'
+        }
+    ];
+
+    return (
+        <div className='w-48 h-full sticky bg-white text-black p-2 '>
+            <div className='text-2xl text-center'>Sales Prediction</div>
+            <ul className='w-full h-96 mt-10 flex items-start  gap-5 justify-center flex-col'>
+                {data.map((item, index) => {
+                    return (
+                        <li key={index} className='w-full h-10 flex justify-start items-center pl-2 rounded-lg hover:bg-blue-300 p-2 '>
+                            <Link
+                                className='flex w-full h-full items-center gap-5'
+                                href={item.route}>
+                                {item.icon} {item.title}
+                            </Link>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
 };
 
 export default Sidebar;
